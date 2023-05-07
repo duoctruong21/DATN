103a9d3865e7c2bd8961e733775b36842726c2ac
duoctruong21 <112966857+duoctruong21@users.noreply.github.com>
2023-05-07 18:48:55 +0700

    update controller components

---
+++ WebMusic/WebMusic/Controllers/ComponentsController.cs
@@ -0,0 +1,47 @@
+﻿using FirebaseAdmin.Messaging;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using WebMusic.Common;
+using WebMusic.models.ef;
+using WebMusic.Models;
+
+namespace WebMusic.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ComponentsController : ControllerBase
+    {
+        
+        private readonly MusicWebContext _context;
+
+        public ComponentsController(MusicWebContext context)
+        {
+            _context = context;
+        }
+        //get item song
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<SongItem>>> GetItem()
+        {
+            try {
+                List<SongItem> items = await (
+                from song in _context.Songs
+                join singer in _context.Singers on song.IdSinger equals singer.Id 
+                join album in _context.Albums on song.IdAlbum equals album.Id into albumItem
+                from albums in albumItem.DefaultIfEmpty()
+                select new SongItem
+                {
+                    songName = song.SongName,
+                    singerName = singer.SingerName,
+                    albumName = albums.AlbumName,
+                    fileImg = song.Fileimg
+                }).Take(5).ToListAsync();
+                return items;
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(ex.Message);
+            }
+        }
+    }
+}
---
+++ WebMusic/WebMusic/Models/SongItem.cs
@@ -0,0 +1,10 @@
+﻿namespace WebMusic.Models
+{
+    public class SongItem
+    {
+        public string? songName { get; set; }
+        public string? singerName { get; set; }
+        public string? albumName { get; set; }
+        public string? fileImg { get; set; }
+    }
+}
